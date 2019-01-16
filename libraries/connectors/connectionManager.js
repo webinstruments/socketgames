5e463fc956b9449278e38990e3ec773e1680c104
Oliver <blablubed@gmai.com>
2019-01-16 16:07:33 +0100

    fixed connection problem for mobile devices - hard coded connection url

--- libraries/connectors/connectionManager.js
+++ libraries/connectors/connectionManager.js
@@ -1,24 +1,7 @@
 function ConnectionManager(url, callbacks, output) {
-    this.onErrorClient = callbacks.onError;
-    this.clientBinder = callbacks.binder;
-    if(!this.clientBinder) {
-        this.clientBinder = null;
-    }
-    callbacks.onError = this.onError;
-    callbacks.binder = this;
     this.callbacks = callbacks;
     this.output = output;
     this.connect(url);
-}
-
-ConnectionManager.prototype.onError = function(err) {
-    if(this.tries == 0) {
-        this.onErrorClient(err);
-        this.tries++;
-        this.connection = new SocketIOConnection(this.url, this.callbacks, this.output);
-    } else {
-        this.onErrorClient.call(this.clientBinder, err);
-    }
 }
 
 ConnectionManager.prototype.getUrl = function() {
@@ -30,11 +13,11 @@ ConnectionManager.prototype.connect = function(url) {
     //Ist dies nicht möglich, dann mit socket io
     //bei Wechsel der Verbindung wieder von vorne
     this.url = url;
-    this.tries = 0;
-    if(this.connection) {
-        this.connection = null;
+    if(url === 'ws://193.171.127.8:8080/ws') {
+        this.connection = new SocketConnection(url, this.callbacks, this.output);
+    } else {
+        this.connection = new SocketIOConnection(url, this.callbacks, this.output);
     }
-    this.connection = new SocketConnection(url, this.callbacks, this.output);
 }
 
 ConnectionManager.prototype.send = function(msg, measurement) {
