abf378898d5464f56df55b7336edc1b3fcf2c89b
OliWo <blablubed@gmail.com>
2019-01-22 20:15:07 +0100

    changed version

--- tetris/components/globals.js
+++ tetris/components/globals.js
@@ -21,7 +21,7 @@ var gameGlobals = {
     scoreController: null,
     stats: null,
     control: null,
-    version: "1.22",
+    version: "1.23",
     //init
     camera: null,
     orthoCamera: null,
